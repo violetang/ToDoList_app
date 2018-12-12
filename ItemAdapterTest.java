23945825f61696a85f3e3379333354b989aa8d6f
violetang <34366585+violetang@users.noreply.github.com>
2018-12-12 11:24:58 -0800

    Add files via upload

---
+++ ItemAdapterTest.java
@@ -0,0 +1,102 @@
+package com.example.violetang.navigationbuttom;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ItemAdapterTest extends BaseAdapter {
+
+    LayoutInflater mInflater;
+    ArrayList<String> task_items;
+    ArrayList<String> task_descriptions;
+    ArrayList<Integer> task_id;
+    ArrayList<Integer> task_status;
+
+    private DatabaseHelper myDB;
+
+    public ItemAdapterTest(Context c, ArrayList<Integer> id, ArrayList<String> i, ArrayList<String> d, ArrayList<Integer> status) {
+        task_id = id;
+        task_items = i;
+        task_descriptions = d;
+        task_status = status;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return task_items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return task_items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = mInflater.inflate(R.layout.item_task, null);
+        myDB = new DatabaseHelper(MyApplication.getAppContext());
+
+        TextView nameTextView = (TextView) v.findViewById(R.id.taskName_TextView);
+        TextView decriptionTextView = (TextView) v.findViewById(R.id.taskDes_TextView);
+        final CheckBox taskCheckBox = (CheckBox) v.findViewById(R.id.taskCheckBox);
+
+        final int index = position;
+
+        String name = task_items.get(position);
+        String desc = task_descriptions.get(position);
+        int status = task_status.get(position);
+
+        nameTextView.setText(name);
+
+        decriptionTextView.setText(desc);
+        if (status == 1) {
+            taskCheckBox.setChecked(true);
+        } else {
+            taskCheckBox.setChecked(false);
+        }
+
+        taskCheckBox.setOnClickListener(new View.OnClickListener() {
+
+            int id = task_id.get(index);
+
+            @Override
+            public void onClick(View v) {
+                if(taskCheckBox.isChecked()){
+                    myDB.setTaskComplete(id);
+                    Toast.makeText(MyApplication.getAppContext(),"set Complete "+id,Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    myDB.setTaskIncomplete(id);
+                    Toast.makeText(MyApplication.getAppContext(),"set unComplete "+id,Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+        return v;
+    }
+
+}
---
+++ ListFragment.java
@@ -0,0 +1,289 @@
+package com.example.violetang.navigationbuttom;
+
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+    DatabaseHelper myDB;
+
+    ImageView setting;
+    ImageView addNewTask;
+    ImageView addList;
+
+    ListAdapter listItemAdapter;
+
+    ListView listBar;
+    ListView tasksOfTheList_todo;
+    ListView getTasksOfTheList_complete;
+
+    TextView listbar;
+
+    int list_ID;
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        myDB = new DatabaseHelper(getActivity());
+
+        Resources res =getResources();
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        listbar = (TextView)view.findViewById(R.id.listbar_title);
+
+        //Tool bar
+        setting = (ImageView)view.findViewById(R.id.listbar_setting);
+        setting();
+
+        addNewTask = (ImageView)view.findViewById(R.id.listbar_addNewList);
+        addListTask();
+
+        addList =(ImageView)view.findViewById(R.id.addlist_imageview);
+        addlist();
+
+        //right task bar
+        tasksOfTheList_todo = (ListView)view.findViewById(R.id.taskBartodo_ListView);
+        getTasksOfTheList_complete = (ListView)view.findViewById(R.id.taskBarcomplete_ListView);
+
+        //left list Bar
+        listBar = (ListView)view.findViewById(R.id.listNavBar);
+        listbarCreated(listBar);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onResume(){
+
+        super.onResume();
+        listbarCreated(listBar);
+        listItemAdapter.notifyDataSetChanged();
+
+    }
+
+
+    public static void setDynamicHeight(ListView listView) {
+        ItemAdapterTest adapter = (ItemAdapterTest)listView.getAdapter();
+        //check adapter if null
+        if (adapter == null) {
+            return;
+        }
+        int height = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            height += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams layoutParams = listView.getLayoutParams();
+        layoutParams.height = height + (listView.getDividerHeight() * (adapter.getCount() - 1));
+        listView.setLayoutParams(layoutParams);
+        listView.requestLayout();
+    }
+
+
+    public void addlist(){
+        addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Addlist_Intent = new Intent(addList.getContext(),AddListActivity.class);
+                startActivity(Addlist_Intent);
+            }
+        });
+    }
+
+    public void addListTask(){
+        addNewTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Addtask_Intent = new Intent(addNewTask.getContext(),ListAddTask.class);
+                startActivity(Addtask_Intent);
+            }
+        });
+    }
+
+    public void setting(){
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (v.getId()) {
+                    case R.id.listbar_setting:
+
+                        PopupMenu popup = new PopupMenu(MyApplication.getAppContext(), v);
+                        popup.getMenuInflater().inflate(R.menu.titlebar_settingmenu, popup.getMenu());
+                        popup.show();
+                        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.titleBar_Setting_Pinfo:
+                                        Intent PersonInfo_Intent = new Intent(setting.getContext(),personalInfoActivity.class);
+                                        startActivity(PersonInfo_Intent);
+
+                                        break;
+
+                                    case R.id.titleBar_Setting_About:
+                                        //Toast.makeText(MyApplication.getAppContext(), item.getTitle(), Toast.LENGTH_LONG).show();
+                                        Intent About_Intent = new Intent(setting.getContext(), AboutActivity.class);
+                                        //send any info to about page
+                                        startActivity(About_Intent);
+
+                                        break;
+
+                                    default:
+                                        break;
+                                }
+                                return true;
+                            }
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    public void listbarCreated(ListView listItems){
+
+        final ArrayList<Integer> myListId = new ArrayList<>();
+        final ArrayList<String> list_Name = new ArrayList<>();
+
+        Cursor getMyList = myDB.getLists();
+        while (getMyList.moveToNext()) {
+
+            myListId.add(getMyList.getInt(0));
+            list_Name.add(getMyList.getString(1));
+
+        }
+
+        listItemAdapter = new ListAdapter(MyApplication.getAppContext(),myListId,list_Name);
+        listItems.setAdapter(listItemAdapter);
+
+        listItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //get the list_id
+                list_ID = myListId.get(position);
+
+                Intent list_detail = new  Intent(view.getContext(),ListDetail.class);
+                list_detail.putExtra("LIST_ID",list_ID);
+                startActivity(list_detail);
+                return true;
+            }
+        });
+
+        listItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setSelected(true);
+                list_ID = myListId.get(position);
+                listbar.setText(list_Name.get(position));
+                //Toast.makeText(MyApplication.getAppContext(), "List" + list_id + "Clicked", Toast.LENGTH_LONG).show();
+
+                final ArrayList<Integer> todoTasksId = new ArrayList<>();
+                ArrayList<String> todoTasks = new ArrayList<>();
+                ArrayList<String> todoTasksDes = new ArrayList<>();
+                ArrayList<Integer> taskStatus = new ArrayList<>();
+
+                //database query: get corresponding task by list_id
+                Cursor list_task = myDB.getTaskByList(list_ID,0);
+
+                while(list_task.moveToNext()) {
+                    todoTasksId.add(list_task.getInt(0));
+                    todoTasks.add(list_task.getString(1));
+                    todoTasksDes.add(list_task.getString(2));
+                    taskStatus.add(list_task.getInt(4));
+                }
+                ItemAdapterTest taskItemAdapter = new ItemAdapterTest(MyApplication.getAppContext(),todoTasksId,todoTasks,todoTasksDes,taskStatus);
+                tasksOfTheList_todo.setAdapter(taskItemAdapter);
+                setDynamicHeight(tasksOfTheList_todo); //set dynmic height for listview
+
+                //listviewClicksetting(tasksOfTheList_todo,list_ID,0);
+
+
+                final ArrayList<Integer> ComTasksId = new ArrayList<>();
+                ArrayList<String> completeTask = new ArrayList<>();
+                ArrayList<String> completeTaskDes = new ArrayList<>();
+                ArrayList<Integer> CompletetaskStatus = new ArrayList<>();
+
+                Cursor list_task_completed = myDB.getTaskByList(list_ID,1);
+
+                while(list_task_completed.moveToNext()) {
+                    ComTasksId.add(list_task_completed.getInt(0));
+                    completeTask.add(list_task_completed.getString(1));
+                    completeTaskDes.add(list_task_completed.getString(2));
+                    CompletetaskStatus.add(list_task_completed.getInt(4));
+                }
+                ItemAdapterTest taskItemAdapter2 = new ItemAdapterTest(MyApplication.getAppContext(),ComTasksId,completeTask,completeTaskDes,CompletetaskStatus);
+                getTasksOfTheList_complete.setAdapter(taskItemAdapter2);
+                setDynamicHeight(getTasksOfTheList_complete); //set dynmic height for listview
+
+                //listviewClicksetting(getTasksOfTheList_complete,list_ID,1);
+            }
+        });
+
+    }
+
+    public void listviewClicksetting(ListView temp, final int listID, final int status){
+
+        temp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                final ArrayList<Integer> TasksId = new ArrayList<>();
+
+                //database query: get corresponding task by list_id
+                Cursor list_task = myDB.getTaskByList(listID,status);
+                while(list_task.moveToNext()) {
+                    TasksId.add(list_task.getInt(0));
+                }
+
+                int taskid = TasksId.get(position);
+
+                Toast.makeText(MyApplication.getAppContext(), "task" + taskid + "Clicked", Toast.LENGTH_LONG).show();
+                Intent task_detail = new Intent(view.getContext(), ItemDetail_List.class);
+                task_detail.putExtra("LIST_TASK_ID",taskid);
+                startActivity(task_detail);
+            }
+        });
+
+
+    }
+}
